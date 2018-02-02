7661b795cc64461dc94d30f53ff5a063d783ddc6
Bear <lihheng@lihs-macbook-air.local>
2018-02-02 09:43:36 +0800

    Initial Commit

---
+++ ThirdAssessment-LihHeng/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThirdAssessment-LihHeng
+//
+//  Created by Lih Heng Yew on 02/02/2018.
+//  Copyright © 2018 Lih Heng Yew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
